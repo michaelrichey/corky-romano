dc9c1ca4703409920e4b3de61f6d3971b9a66a3c
michaelrichey <michael@tofugu.com>
2020-03-12 13:41:46 -0400

    simplified function

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,9 +4,5 @@ console.log('log me log')
 document.querySelector("#menu").addEventListener('click', function (e) {
     document.querySelector("#menu-grid-1").innerHTML = ''
 
-
-    const showClippy = document.createElement('div')
-    showClippy.classList.add("show-clippy")
-    showClippy.innerHTML = "<div><p>It looks like you're trying to click a link. Would you like help?</p><img src=\"images/clippy.gif\"></div>"
-    document.querySelector("#menu-grid-1").appendChild(showClippy)
+    document.querySelector("#menu-grid-1").innerHTML = "<div class=\"show-clippy\"><p>It looks like you're trying to click a link. Would you like help?</p><img src=\"images/clippy.gif\"></div>"
 })
