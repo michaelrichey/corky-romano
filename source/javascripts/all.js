2a993c447391f9760ccc07ce0342b3805f09d9e1
michaelrichey <michael@tofugu.com>
2020-03-17 17:43:35 -0400

    prettier formatting in js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,9 @@
 //= require_tree .
 
-console.log('log me log')
-document.querySelector("#menu").addEventListener('click', function (e) {
-    document.querySelector("#menu-grid-1").innerHTML = ''
+console.log("log me log");
+document.querySelector("#menu").addEventListener("click", function(e) {
+  document.querySelector("#menu-grid-1").innerHTML = "";
 
-    document.querySelector("#menu-grid-1").innerHTML = "<div class=\"show-clippy\"><p>It looks like you're trying to click a link. Would you like help?</p><img src=\"../images/clippy.gif\"></div>"
-})
+  document.querySelector("#menu-grid-1").innerHTML =
+    '<div class="show-clippy"><p>It looks like you\'re trying to click a link. Would you like help?</p><img src="../images/clippy.gif"></div>';
+});
