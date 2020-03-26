5a3c7538c3a2862a2f2cd0d573c6294a647e9676
michaelrichey <michael@tofugu.com>
2020-03-26 13:36:39 -0400

    add info with ruby helpers

--- lib/site_helpers.rb
+++ lib/site_helpers.rb
@@ -1,7 +1,14 @@
 module SiteHelpers
-    def log_movie_tiles
+
+    def log_movie_titles
       data.halloweenmovies.each do |title|
         puts title
       end
+    end
+
+    def detailed_info
+        data.halloweenmovies.each do |film|
+            film
+        end
     end
 end
