70aa954e263e96bde7ca4959c82fd57785212af8
michaelrichey <michael@tofugu.com>
2020-04-29 11:21:30 -0400

    Revert middleman deploy
    This reverts commit 60a444ad3098c8ec20ae61dd2b887196ec8d97ca.
    
    # Conflicts:
    #	config.rb

--- config.rb
+++ config.rb
@@ -112,9 +112,3 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
-
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  deploy.branch = 'master'
-  deploy.build_before = true
-end
