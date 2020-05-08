2a534321fee5ca1ffef794dce0b348f94b3fd397
michaelrichey <michael@tofugu.com>
2020-05-08 18:12:29 -0400

    add javascript to get event value from form

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,24 @@
 //= require_tree .
 
 console.log("log me log");
-document.querySelector("#menu").addEventListener("click", function(e) {
+
+document.querySelector("#menu").addEventListener("click", function (e) {
   document.querySelector("#menu-grid-1").innerHTML = "";
 
   document.querySelector("#menu-grid-1").innerHTML =
     '<div class="show-clippy"><p>It looks like you\'re trying to click a link. Would you like help?</p><img src="../images/clippy.gif"></div>';
 });
+
+// for quiz
+
+document.querySelector('#quiz').addEventListener('submit', function (e) {
+  e.preventDefault()
+  console.log(e.target.elements[0].value)
+})
+/*
+document.querySelector('#quizbutton').addEventListener('click', function (e) {
+  e.preventDefault()
+  console.log(e)
+
+})
+*/
