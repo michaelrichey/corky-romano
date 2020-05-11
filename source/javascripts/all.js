d2a4ea2d3de22eceb5bad8e000a1b0339dc28894
michaelrichey <michael@tofugu.com>
2020-05-11 12:09:16 -0400

    add array and push to array

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,9 +11,24 @@ document.querySelector("#menu").addEventListener("click", function (e) {
 
 // for quiz
 
+let answers = [
+  {
+    a1: "asdf",
+    a2: "asdf",
+    a3: "asdf",
+    a4: "asdf",
+    a5: "asdf",
+  }
+]
+
 document.querySelector('#quiz').addEventListener('submit', function (e) {
   e.preventDefault()
   console.log(e.target.elements[0].value)
+  answers[0].a1 = e.target.elements[0].value
+
+  console.log('this is ' + answers[0].a1)
+
+
 })
 /*
 document.querySelector('#quizbutton').addEventListener('click', function (e) {
