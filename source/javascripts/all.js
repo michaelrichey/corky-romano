31f7f05f9c0e4e4f861796edafa5ef0c642df7fb
michaelrichey <michael@tofugu.com>
2020-05-12 12:46:28 -0400

    Revert "destructured again"
    This reverts commit 504fff4851d973607003dd7f689bc264976d45d1.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -61,7 +61,17 @@ let questioninfo = [
   }
 ]
 
-$.each(questioninfo, function (index, { questionid, answerid, message, buttonid, question, answer, number }) {
+$.each(questioninfo, function (index, item) {
+
+  let {
+    questionid,
+    answerid,
+    message,
+    buttonid,
+    question,
+    answer,
+    number
+  } = item
 
   $(questionid).append(`<p>${question}</p>`)
 
