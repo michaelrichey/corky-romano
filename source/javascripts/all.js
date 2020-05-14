b243e76510027599b63b9983672f58ceef188851
michaelrichey <michael@tofugu.com>
2020-05-14 18:50:56 -0400

    partial refactor

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -15,78 +15,59 @@ let answers = []
 
 let questioninfo = [
   {
-    questionid: '#question1',
-    answerid: "#answer1",
-    message: "#message1",
-    buttonid: "#button1",
     question: "What is the first temple you have to beat as Adult Link in The Legend of Zelda: Ocarina of Time?",
     answer: "forest temple",
-    number: "1"
   },
   {
-    questionid: '#question2',
-    answerid: "#answer2",
-    message: "#message2",
-    buttonid: "#button2",
     question: "Which member of musical duo They Might Be Giants plays guitar?",
     answer: "john flansburgh",
-    number: "2"
   },
   {
-    questionid: '#question3',
-    answerid: "#answer3",
-    message: "#message3",
-    buttonid: "#button3",
     question: "What cereal company is best known for licensed character cereals like Batman, Ghostbusters, Barbie, Cabbage Patch Kids, WWF, and Nintendo (and they were also a subsidiary of pet food company Purina)?",
     answer: "ralston",
-    number: "3"
   },
   {
-    questionid: '#question4',
-    answerid: "#answer4",
-    message: "#message4",
-    buttonid: "#button4",
     question: "What Microsoft console is sometimes affectionately called 'The Dreamcast 2' because of its large number of Sega exclusives that were originally meant for the Dreamcast?",
     answer: "xbox",
-    number: "4"
   },
   {
-    questionid: '#question5',
-    answerid: "#answer5",
-    message: "#message5",
-    buttonid: "#button5",
     question: "What Teddy Grahams spin-off cookie was superior to Dunkaroos, but is not getting a revival is 2020 for God knows what reason?",
     answer: "dizzy grizzlies",
-    number: "5"
   }
 ]
 
 $.each(questioninfo, function (index, item) {
 
+  let number = index + 1
+
   let {
-    questionid,
-    answerid,
-    message,
-    buttonid,
     question,
-    answer,
-    number
+    answer
   } = item
 
-  $(questionid).append(`<p>${question}</p>`)
+  let container = $('.question-and-answer').eq(index)
+  let form = container.find('form')
+  let header = container.find('h2')
+  let inputText = container.find('input[type="text"]')
+  let button = container.find('input[type="submit"]')
+  let message = container.find('.message')
 
-  $(answerid).on('submit', function (e) {
+  header.text(`Question ${number}`)
+
+  $(`<p>${question}</p>`).insertAfter(header)
+
+  form.on('submit', function (e) {
     e.preventDefault();
     let typedAnswer = e.target.elements[0].value;
     $('#answers').append(`<p>Answer to question ${number}: ${typedAnswer}</p>`);
 
     if (typedAnswer.toLowerCase() == answer.toLowerCase()) {
       answers.push(typedAnswer);
-      $(message).append(`<p>You got it right!</p>`);
+      message.append(`<p>You got it right!</p>`);
 
-      $(buttonid).prop('disabled', true);
+      button.prop('disabled', true);
     } else {
-      $(message).append(`<p>You got it wrong. Try again.</p>`)
+      message.append(`<p>You got it wrong. Try again.</p>`)
     }
   })
 })
